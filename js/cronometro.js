17c85eeb4696da73ad25cdec7f88ecebf97d99fb
AndreaAcero <158502529+AndreaAcero@users.noreply.github.com>
2025-11-04 19:39:27 +0100

    sesion 7

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,3 +1,74 @@
-class cronometro{
+class Cronometro{
+    constructor(){
+        this.tiempo = 0;
+        this.inicio = null;
+        this.corriendo = null;
+        this.usandoTemporal = false; 
+    }
     
+    arrancar(){
+        try {
+            this.corriendo = setInterval(this.actualizar.bind(this), 100);
+            // Comprobamos si el objeto Temporal existe
+            if (typeof Temporal !== "undefined" && Temporal.Now) {
+                // Si está disponible, usamos Temporal para obtener el instante actual
+                this.inicio = Temporal.Now.instant();
+                this.usandoTemporal = true;
+            } else {
+                // Si no está disponible, lanzamos un error para pasar al catch
+                throw new Error("Temporal no disponible");
+            }
+        } catch (error) {
+            // Fallback: usamos el objeto Date
+            this.inicio = new Date();
+        }
+
+        console.log("Cronómetro arrancado en:", this.inicio);
+    }
+
+    actualizar(){
+        let ahora;
+        //if (this.corriendo) return;
+        try{
+            if (this.usandoTemporal) {
+                ahora = Temporal.Now.instant();
+                this.tiempo = ahora.epochMilliseconds - this.inicio.epochMilliseconds;
+            } else {
+                const ahora = new Date();
+                this.tiempo = ahora - this.inicio;
+            }
+        } catch (error) {
+            console.error("Error al actualizar el cronómetro:", error);
+        }
+        
+        console.log("Tiempo transcurrido (ms):", this.tiempo);
+    }
+
+    mostrar(){
+        let totalMilisegundos = this.tiempo;
+        let minutos = parseInt(totalMilisegundos / 60000); // 1 minuto = 60000 ms
+        let segundos = parseInt((totalMilisegundos % 60000) / 1000); // segundos restantes
+        let decimas = parseInt((totalMilisegundos % 1000) / 100); // décimas de segundo
+        let mm = String(minutos).padStart(2, '0');
+        let ss = String(segundos).padStart(2, '0');
+        let sDecima = String(decimas);
+        let tiempoFormateado = `${mm}:${ss}.${sDecima}`;
+        document.querySelector('main p').textContent = tiempoFormateado;
+    }
+
+    parar() {
+        this.mostrar();
+        clearInterval(this.corriendo);
+        this.corriendo = null; 
+    }
+
+    reiniciar(){
+        clearInterval(this.corriendo);
+        this.corriendo=null;
+        this.tiempo=0;
+        this.mostrar();
+    }
+
+
+
 }
--- js/memoria.js
+++ js/memoria.js
@@ -1,11 +1,90 @@
 class Memoria {
     constructor() {
         console.log("Juego de memoria inicializado");
+        this.tablero_bloqueado = true;
+        this.primera_carta = null;
+        this.segunda_carta = null;
+        this.barajarCartas();
+        this.tablero_bloqueado = false;
+        this.cronometro = new Cronometro();
+        this.cronometro.arrancar();
     }
 
-    voltearCarta(card) {
-        card.setAttribute("data-Estado", "revelado");
-         console.log("Carta volteada:", card);
+    voltearCarta(carta) {
+        if (this.tablero_bloqueado) return;
+        if(carta.classList.contains("revelada")) return;
+        if(carta.getAttribute("data-Estado") === "volteada") return;
+
+        carta.setAttribute("data-Estado", "volteada");
+        if (!this.primera_carta) {
+            this.primera_carta = carta;
+            return;
+        }
+        this.segunda_carta = carta;
+        this.comprobarPareja();
+
+
     }
+
+    barajarCartas(){
+        const contenedor = document.querySelector('main');
+
+        // Obtener todas las cartas como array
+        const cartas = Array.from(contenedor.querySelectorAll('article'));       
+        for(let i = cartas.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            contenedor.appendChild(cartas[j]);
+            cartas[j] = cartas[i];
+        }
+    }
+
+    reiniciarTablero(){
+        this.tablero_bloqueado = true;
+        this.primera_carta = null;
+        this.segunda_carta = null;
+        this.tablero_bloqueado = false;
+    }
+
+    deshabilitarCartas(){
+        if(this.primera_carta && this.segunda_carta){
+           this.primera_carta.classList.add("revelada");
+            this.segunda_carta.classList.add("revelada");
+            this.reiniciarTablero();
+        }
+    }
+
+     comprobarJuego() {
+        const cartas = document.querySelectorAll('main article');
+        const todasReveladas = Array.from(cartas).every(carta => carta.classList.contains('revelada'));
+
+        if (todasReveladas) {
+            alert("¡Felicidades! Has completado el juego de memoria.");
+        }
+        this.cronometro.parar();
+    }
+
+    cubrirCartas(){
+        this.tablero_bloqueado = true;
+        setTimeout(() => {
+            if(this.primera_carta && this.segunda_carta){
+                this.primera_carta.setAttribute("data-Estado", "oculto");
+                this.segunda_carta.setAttribute("data-Estado", "oculto");
+            }
+             this.reiniciarTablero();
+
+        }, 1500);
+    }
+
+     comprobarPareja() {
+        if (!this.primera_carta || !this.segunda_carta) return;
+
+        const imgPrimera = this.primera_carta.querySelector('img').getAttribute('src');
+        const imgSegunda = this.segunda_carta.querySelector('img').getAttribute('src');
+
+        (imgPrimera === imgSegunda) ? this.deshabilitarCartas() : this.cubrirCartas();
+    }
+
+
+
 }
 const juegoMemoria = new Memoria();
